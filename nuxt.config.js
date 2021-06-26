37f462212958ba4ff421adc0e60c6bfe043855c4
Ali Kompany <alii.cmp@gmail.com>
2021-06-26 21:57:13 +0430

    added resume files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+
+  target: 'static',
+  generate: {
+    subFolders: true
+  },
+  router: {
+    base: '/'
+  },
+
   head: {
     title: 'landing-v2',
     htmlAttrs: {
